dc400de52e2c9d1324f4fa6756816cd29ffdaea7
celil öztürk <celil.ozturk@windowslive.com>
2018-05-16 16:40:59 +0300

    a
    aa

---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.LightGray;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ADD STORY";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.LightGray;
+            this.button2.Location = new System.Drawing.Point(12, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 70);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ADD TASK";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp6.Properties.Resources.arkaplan2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1200, 671);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SCRUM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+          
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach(Control a in this.Controls)
+            {
+                if (a is Panel && a.Width == 137)
+                {
+                    if (a.Location.X > 280 && a.Location.X < 530 && a.Location.Y>190)
+                        a.BackColor = Color.Tomato;
+                    else if(a.Location.X > 529 && a.Location.X < 830 && a.Location.Y > 190)
+                        a.BackColor = Color.Gold;
+                    else if (a.Location.X > 829 && a.Location.X < 1200 && a.Location.Y > 190)
+                        a.BackColor = Color.SeaGreen;
+                    else
+                        a.BackColor = Color.DarkGray;
+                }
+                if (a is Panel && a.Width == 137)
+                {
+                    if (a.Location.X > 960 && a.Location.Y < 80)
+                        this.Controls.Remove(a);
+                   
+                }
+                if (a is Panel && a.Width == 230)
+                {
+                    if (a.Location.X > 870 && a.Location.Y < 80)
+                        this.Controls.Remove(a);
+
+                }
+                if (a is Panel && a.Width == 230)
+                {
+                    if ( a.Location.X < 85 && a.Location.Y > 190)
+                        a.BackColor = Color.SkyBlue;
+                    else
+                        a.BackColor = Color.DarkGray;
+                }
+
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Panel pnl = new Panel();
+            pnl.Location = new Point(532, 40);
+            pnl.Width = 137;
+            pnl.Height = 110;
+            pnl.BackColor = Color.DarkGray;
+            ControlExtension.Draggable(pnl, true);
+            Label lbl1 = new Label();
+            lbl1.Text = "Story No:";
+            lbl1.Location = new Point(3, 5);
+            lbl1.Width = 55;
+            lbl1.Height = 13;
+            Label lbl2 = new Label();
+            lbl2.Text = "End Date:";
+            lbl2.Location = new Point(3, 26);
+            NumericUpDown nud1 = new NumericUpDown();
+           nud1.Location = new Point(55, 3);
+            nud1.Width = 30;
+            nud1.Height = 20;
+            DateTimePicker dtp = new DateTimePicker();
+            dtp.Format = DateTimePickerFormat.Short;
+            dtp.Location = new Point(55, 24);
+            dtp.Width = 80;
+            dtp.Height = 20;
+            TextBox txt1 = new TextBox();
+            txt1.Width = 131;
+            txt1.Height = 20;
+            txt1.Text = "Employee";
+            txt1.Location = new Point(3, 46);
+            TextBox txt2 = new TextBox();
+            txt2.Width = 131;
+            txt2.Height = 39;
+            txt2.Text = "Task";
+            txt2.Location = new Point(3, 68);
+            txt2.Multiline = true;
+            pnl.Controls.Add(txt2);
+            pnl.Controls.Add(txt1);
+            pnl.Controls.Add(dtp);
+            pnl.Controls.Add(nud1);
+            pnl.Controls.Add(lbl1);
+            pnl.Controls.Add(lbl2);
+            this.Controls.Add(pnl);
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Panel pnl1 = new Panel();
+            pnl1.Location = new Point(485, 20);
+            pnl1.Width = 230;
+            pnl1.Height = 140;
+            pnl1.BackColor = Color.DarkGray;
+            ControlExtension.Draggable(pnl1, true);
+            Label lbl1 = new Label();
+            lbl1.Text = "Story No:";
+            lbl1.Location = new Point(3, 5);
+            lbl1.Width = 55;
+            lbl1.Height = 13;
+            
+            NumericUpDown nud1 = new NumericUpDown();
+            nud1.Location = new Point(61, 3);
+            nud1.Width = 30;
+            nud1.Height = 20;
+            TextBox txt2 = new TextBox();
+            txt2.Width = 224;
+            txt2.Height = 90;
+            txt2.Text = "Description";
+            txt2.Location = new Point(3, 45);
+            txt2.Multiline = true;
+            Label lbl2 = new Label();
+            lbl2.Text = "Date:";
+            lbl2.Location = new Point(3, 26);
+            lbl2.Width = 55;
+            lbl2.Height = 13;
+            DateTimePicker dtp = new DateTimePicker();
+            dtp.Format = DateTimePickerFormat.Short;
+            dtp.Location = new Point(61, 24);
+            dtp.Width = 80;
+            dtp.Height = 20;
+            TextBox txt1 = new TextBox();
+            txt1.Width = 80;
+            txt1.Height = 20;
+            txt1.Text = "Tag";
+            txt1.Location = new Point(100,3);
+            pnl1.Controls.Add(txt2);
+            pnl1.Controls.Add(nud1);
+            pnl1.Controls.Add(lbl1);
+            pnl1.Controls.Add(lbl2);
+            pnl1.Controls.Add(txt1);
+            pnl1.Controls.Add(dtp);
+            this.Controls.Add(pnl1);
+        }
+    }
+}
